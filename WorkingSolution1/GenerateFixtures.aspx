e6eeba4ff5ed75709f302effb87b2e71219350ed
Kabelo <mamadi_k@rocketmail.com>
2014-08-15 19:42:59 +0200

    Generation of fixtures

---
+++ WorkingSolution1/GenerateFixtures.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GenerateFixtures.aspx.cs" Inherits="GenerateFixtures" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:DropDownList ID="DropDownList1" runat="server" 
+            DataSourceID="SqlDataSource1" DataTextField="LeagueName" 
+            DataValueField="LeagueName">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:EsportsConnectionString2 %>" 
+            SelectCommand="SELECT [LeagueName] FROM [SetupLeague]"></asp:SqlDataSource>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" onclick="Button1_Click" 
+            Text="Generate Fixtures" />
+        <br />
+        <br />
+      <a href="Default.aspx">
+            Go back to home page
+        </a>
+    
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+            DataSourceID="SqlDataSource2" Enabled="False" Visible="False">
+            <Columns>
+                <asp:BoundField DataField="TeamName" HeaderText="TeamName" 
+                    SortExpression="TeamName" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource2" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:EsportsConnectionString %>" 
+            SelectCommand="SELECT [TeamName] FROM [SetupTeams]"></asp:SqlDataSource>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WorkingSolution1/GenerateFixtures.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class GenerateFixtures : System.Web.UI.Page
+{
+    public int gameweeks = 0;
+    public int gamesPerWeek = 0;
+    public int numberOfFixtures = 0;
+    private SqlConnection sqlCon = new SqlConnection(@"Data Source=localhost;Initial Catalog=Esports;Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Text
+        sqlCon.Open();
+        numberOfFixtures = 2;
+        Label1.Visible = true;
+
+    }
+    public string Home { get; set; }
+    public string Away { get; set; }
+
+    void CallCode(int LeagueID)
+    {
+        string[] teamss = new string[GridView1.Rows.Count];
+        gameweeks = GridView1.Rows.Count;
+        gamesPerWeek = GridView1.Rows.Count;
+        for (int i = 0; i < GridView1.Rows.Count; i++)
+        {
+           teamss[i] = GridView1.Rows[i].Cells[0].Text;
+        }
+        List<GenerateFixtures> fixtures = CalculateFixtures(teamss);
+        int a = 0;
+        for (int i = 0; i < fixtures.Count; i++)
+        {
+            a = a + 1;
+            if (a <= gamesPerWeek)
+            {
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO Fixtures VALUES ('" + fixtures[i].Home + "','" + fixtures[i].Away + "','" + "Week 1" + "')", sqlCon);
+                sqlCommand.ExecuteNonQuery();
+            }
+            else if (a <= (gamesPerWeek * 2))
+            {
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO Fixtures VALUES ('" + fixtures[i].Home + "','" + fixtures[i].Away + "','" + "Week 2" + "')", sqlCon);
+                sqlCommand.ExecuteNonQuery();
+            }
+            else if (a <= (gamesPerWeek * 3))
+            {
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO Fixtures VALUES ('" + fixtures[i].Home + "','" + fixtures[i].Away + "','" + "Week 3" + "')", sqlCon);
+                sqlCommand.ExecuteNonQuery();
+            }
+            else if (a <= (gamesPerWeek * 4))
+            {
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO Fixtures VALUES ('" + fixtures[i].Home + "','" + fixtures[i].Away + "','" + "Week 4" + "')", sqlCon);
+                sqlCommand.ExecuteNonQuery();
+            }
+            else if (a <= (gamesPerWeek * 5))
+            {
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO Fixtures VALUES ('" + fixtures[i].Home + "','" + fixtures[i].Away + "','" + "Week 5" + "')", sqlCon);
+                sqlCommand.ExecuteNonQuery();
+            }
+            else if (a <= (gamesPerWeek * 6))
+            {
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO Fixtures VALUES ('" + fixtures[i].Home + "','" + fixtures[i].Away + "','" + "Week 6" + "')", sqlCon);
+                sqlCommand.ExecuteNonQuery();
+            }
+
+        }
+    }
+
+    List<GenerateFixtures> CalculateFixtures(string[] teams)
+    {
+        List<GenerateFixtures> fixtures = new List<GenerateFixtures>();
+        int c = 0;
+        for (int i = 0; i < teams.Length; i++)
+            {
+                for (int j = 0; j < teams.Length; j++)
+                {
+                    if (teams[i] != teams[j])
+                    {
+                        fixtures.Add(new GenerateFixtures() { Home = teams[i], Away = teams[j] });
+                        c = c + 1;
+                    }
+                }
+            }
+        for (int i = 0; i < fixtures.Count; i++)
+        {
+            GenerateFixtures temp = fixtures[i];
+            Random r = new Random();
+            int randomIndex = r.Next(i, fixtures.Count);
+            fixtures[i] = fixtures[randomIndex];
+            fixtures[randomIndex] = temp;
+        }
+
+        return fixtures;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int LeagueID = DropDownList1.SelectedIndex;
+        GridView1.Enabled = true;
+        GridView1.Visible = true;
+        CallCode(LeagueID);
+        Label1.Text = "Fixtures have been generated";
+    }
+}
